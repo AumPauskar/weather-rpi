02b9283694f4ba57cd4972e837b8576d2f64210b
Aum Pauskar <aumpauskar111@gmail.com>
2024-06-17 11:12:59 +0530

    Included multithreaded timers

--- weather-api/app.py
+++ weather-api/app.py
@@ -2,6 +2,8 @@ from flask import Flask, jsonify
 import adafruit_dht
 import board
 from RPi import GPIO
+import threading
+import time
 
 # global constants
 temp_c = 0
@@ -9,33 +11,37 @@ temp_f = 0
 humid  = 0
 
 def get_dht_readings():
-    # previous tempratures in case of error reading temps
     global temp_c, temp_f, humid
-    prev_temp_c = temp_c
-    prev_temp_f = temp_f
-    prev_humid = humid
+    while True:
+        prev_temp_c = temp_c
+        prev_temp_f = temp_f
+        prev_humid = humid
 
-    try:
-        # Initialize the DHT device inside the route function
-        dht_device = adafruit_dht.DHT11(board.D4)
-        temperature_c = dht_device.temperature
-        humidity = dht_device.humidity
+        try:
+            dht_device = adafruit_dht.DHT11(board.D4)
+            temperature_c = dht_device.temperature
+            humidity = dht_device.humidity
+            
+            if temperature_c is not None and humidity is not None:
+                temperature_f = int(temperature_c * (9 / 5) + 32)
+                temp_c = temperature_c
+                temp_f = temperature_f
+                humid = humidity
+            else:
+                temp_c = prev_temp_c
+                temp_f = prev_temp_f
+                humid = prev_humid
+        except RuntimeError as error:
+            print(str(error))
+        finally:
+            dht_device.exit()
         
-        # Check if readings are valid
-        if temperature_c is not None and humidity is not None:
-            temperature_f = int(temperature_c * (9 / 5) + 32)
-            temp_c = temperature_c
-            temp_f = temperature_f
-            humid = humidity
-        else:
-            temp_c = prev_temp_c
-            temp_f = prev_temp_f
-            humid = prev_humid
-    except RuntimeError as error:
-        print(str(error))
-    finally:
-        # Cleanup the sensor after use
-        dht_device.exit()
+        time.sleep(5)  # Wait for 5 seconds before the next read
+
+def start_dht_readings_thread():
+    thread = threading.Thread(target=get_dht_readings)
+    thread.daemon = True  # Daemonize thread
+    thread.start()
 
 app = Flask(__name__)
 
@@ -52,10 +58,9 @@ def get_readings():
         return jsonify({
             'error': str(e)
         }), 500
-    
 
 def main():
-    print(get_dht_readings())
+    start_dht_readings_thread()  # Start the sensor readings update thread
     app.run(host='0.0.0.0', port=5000, debug=True)
     
 if __name__ == '__main__':
