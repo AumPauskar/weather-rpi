2f35728fca182f4c626049cddadde0bcf3fb4acb
Aum Pauskar <aumpauskar111@gmail.com>
2024-06-19 19:40:14 +0530

    Fan on route added

--- weather-api/app.py
+++ weather-api/app.py
@@ -10,6 +10,36 @@ temp_c = 0
 temp_f = 0
 humid  = 0
 
+
+def fan_on():
+
+    gpio = 17
+    enable = 27
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+
+    # enabling the motor/enable pins
+    GPIO.setup(gpio, GPIO.OUT)
+    GPIO.setup(enable, GPIO.OUT)
+
+    # Create a PWM object for the enable pin
+    pwm = GPIO.PWM(enable, 100)
+    pwm.start(100)
+    fan_on = False
+    counter = 10
+    try: 
+        while counter:
+            GPIO.output(gpio, GPIO.HIGH)
+            print(f"Fan on")
+            counter -= 1
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Program stopped via keyboard interrupt")
+        GPIO.output(gpio, GPIO.LOW)
+    finally:
+        # resetting echo
+        GPIO.output(gpio, GPIO.LOW)
+
 def get_dht_readings():
     global temp_c, temp_f, humid
     while True:
@@ -45,6 +75,19 @@ def start_dht_readings_thread():
 
 app = Flask(__name__)
 
+# api route to trigger turning on the fan
+@app.route('/fanon')
+def turn_fan_on():
+    try:
+        fan_on()
+        return jsonify({
+            'message': 'Fan turned on'
+        }), 200
+    except Exception as e:
+        return jsonify({
+            'error': str(e)
+        }), 500
+
 @app.route('/readings')
 def get_readings():
     global temp_c, temp_f, humid
@@ -65,3 +108,4 @@ def main():
     
 if __name__ == '__main__':
     main()
+    # fan_on()
